716b2a00ea9d6a630d79891795c97bfeccb6b8ed
BttCrtd <courtadon.baptiste@gmail.com>
2025-07-17 14:58:58 +0200

    Itialisation du projet, et création de l'objet 3D

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+import Dragon3D from '../components/3D/Dragon3D'
+
+function Home() {
+  return (
+    <div>
+      <Dragon3D />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/3D/Dragon3D.jsx
@@ -0,0 +1,155 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
+
+const Dragon3D = () => {
+  const meshRef = useRef()
+
+  return (
+    <Canvas
+      style={{ height: '400px', width: '100%' }}
+      camera={{ position: [10, 7, 11], fov: 50 }}
+    >
+      <ambientLight intensity={0.2} />
+      <directionalLight position={[0, 0, 5]} />
+      <directionalLight position={[5, 0, 5]} />
+      <directionalLight position={[-5, 0, 5]} />
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[4, 1, 4]} />
+        <meshStandardMaterial color="#111111" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[0, 2, 0]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[4, 1, 4]} />
+        <meshStandardMaterial color="#111111" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[0, 2, -4]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[4, 5, 4]} />
+        <meshStandardMaterial color="#111111" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[1, 2.5, 1]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.8, 0.8, 0.8]} />
+        <meshStandardMaterial color="#111111" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[1, 2.7, 1]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.6, 0.6, 0.6]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[-1, 2.5, 1]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.8, 0.8, 0.8]} />
+        <meshStandardMaterial color="#111111" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[-1, 2.7, 1]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.6, 0.6, 0.6]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      {/****************Yeux***********************/}
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[-1, 3.6, -2.1]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.8, 0.4, 0.4]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[-0.5, 3.5, -2]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.2, 0.2, 0.2]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[1, 3.6, -2.1]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.8, 0.4, 0.4]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[0.5, 3.5, -2]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.2, 0.2, 0.2]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      {/******Langue ************/}
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[0, 0.1, -1.5]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[3, 1, 4]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[0, 0.1, -1]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[2, 1, 4]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[0, 0.1, -0.5]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[1, 1, 4]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      {/***********Cornes ****************/}
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[-1, 4.5, -3]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.8, 1, 0.4]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0.2, 0, 0]} position={[-1, 5, -3.8]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.8, 0.4, 2]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh
+        ref={meshRef}
+        rotation={[0.35, 0.3, 0]}
+        position={[-1.5, 5.2, -4.4]}
+      >
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.8, 0.4, 1.6]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0, 0, 0]} position={[1, 4.5, -3]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.8, 1, 0.4]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh ref={meshRef} rotation={[0.2, 0, 0]} position={[1, 5, -3.8]}>
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.8, 0.4, 2]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      <mesh
+        ref={meshRef}
+        rotation={[0.35, -0.3, 0]}
+        position={[1.5, 5.2, -4.4]}
+      >
+        {/* Forme de base : cube (remplaçable par origami 3D fait de triangles) */}
+        <boxGeometry args={[0.8, 0.4, 1.6]} />
+        <meshStandardMaterial color="violet" roughness={0.5} metalness={0.4} />
+      </mesh>
+
+      {/* Caméra interactive */}
+      <OrbitControls />
+    </Canvas>
+  )
+}
+
+export default Dragon3D
